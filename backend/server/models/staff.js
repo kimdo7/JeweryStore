e51e0758112fe8589954905d9c65722b5d76a153
Kim Do <kim.h.do.seven@gmail.com>
2019-02-27 09:32:26 -0800

    add password

--- backend/server/models/staff.js
+++ backend/server/models/staff.js
@@ -6,6 +6,7 @@ var StaffSchema = new mongoose.Schema({
     last_name: {type: String, required: true, minlength: 2 },
     user_type : {type: Number, required: true},
     user_name : {type: String, required: true, minlength: 2, unique: true},
+    password : {type: String, required: true, minlength: 2},
     email : {type: String, required: true, minlength: 2, unique: true},
     phone : {type: String, required: true, minlength: 2, unique: true},
 }, { timestamps: true })
