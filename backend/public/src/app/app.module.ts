88814c11c766ed1c9497aed1fde1756c66d34c23
Kim Do <kim.h.do.seven@gmail.com>
2019-02-28 09:20:09 -0800

    init angular component staff

--- backend/public/src/app/app.module.ts
+++ backend/public/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StaffComponent } from './staff/staff.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StaffComponent
   ],
   imports: [
     BrowserModule,
